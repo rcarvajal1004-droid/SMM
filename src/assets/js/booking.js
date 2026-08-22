005555aa6b78676d2a1e71406139a78ec8921e35
Rober <rober@localhost>
2026-08-22 01:52:59 -0600

    chore: remove non-Angular files and unused assets

--- src/assets/js/booking.js
+++
@@ -1,4 +0,0 @@
-﻿// Simple interaction for the booking button to show modal
-        document.getElementById('bookBtn').addEventListener('click', function() {
-            document.getElementById('successModal').classList.remove('hidden');
-        });
